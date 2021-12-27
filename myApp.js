2791d3916e64c0a62ebf88d115f91ec1204651cf
Dani Poplin <danih37@gmail.com>
2021-12-27 16:24:45 -0600

    add xss filter

--- myApp.js
+++ myApp.js
@@ -2,11 +2,12 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
-app.use(helmet());
+// app.use(helmet());
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard(
   {action: 'deny'}
 ))
+app.use(helmet.xssFilter())
 
 
 
